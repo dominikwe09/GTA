78d2c751ecf2e992544304b17283dd548407a554
Dominik1934 <dom@weinkamp.de>
2026-06-27 20:31:21 +0200

    chore: add build and launch scripts for distribution

---
+++ build.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+echo Kompiliere...
+if not exist out mkdir out
+javac -cp "lib\GLOOP.jar;lib\gluegen-rt.jar;lib\jogl-all.jar" -d out src\*.java
+if errorlevel 1 (echo Fehler beim Kompilieren & pause & exit /b 1)
+
+echo Erstelle JAR...
+jar -cf GTA.jar -C out .
+echo Fertig! Starte mit start.bat
+pause
---
+++ start.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+java -Djava.library.path=lib\natives\windows-amd64 -cp "GTA.jar;lib\GLOOP.jar;lib\gluegen-rt.jar;lib\jogl-all.jar" GTA
+pause
